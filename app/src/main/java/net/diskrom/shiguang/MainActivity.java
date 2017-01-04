e8f0de4f9278e9061c6349fc5becf107173ceebe
yongbaolinux <yongbaolinux@gmail.com>
2017-01-04 22:57:39 +0800

    123

--- app/src/main/java/net/diskrom/shiguang/MainActivity.java
+++ app/src/main/java/net/diskrom/shiguang/MainActivity.java
@@ -1,13 +1,33 @@
 package net.diskrom.shiguang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
+
+    private Button loadImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        loadImage = (Button) findViewById(R.id.loadImage);
+        loadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                //intent.setPackage("com.miui.gallery");
+                intent.setPackage("com.android.gallery3d");
+                startActivity(intent);
+            }
+        });
     }
 }
