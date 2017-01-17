688a815753e51dcf2545b16a71cbff1987dd28c1
unknown <ekuaibao1@163.com>
2017-01-17 15:11:51 +0800

    约glib殖

--- lib/src/main/java/com/example/MyClass.java
+++ lib/src/main/java/com/example/MyClass.java
@@ -2,6 +2,12 @@ package com.example;
 
 public class MyClass {
     public static void main(String[] args){
-        System.out.println("hello");
+        final String a = "hello";
+        String b = "hello";
+        String c = a+2;
+        String d = b+2;
+        String e = "hello2";
+        System.out.println(e.equals(c));
+        System.out.println(e.equals(d));
     }
 }
