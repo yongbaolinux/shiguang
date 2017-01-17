7cbfaf07b7d4b03e48bb432f09e79c04d46281fd
yongbaolinux <yongbaolinux@gmail.com>
2017-01-17 23:27:47 +0800

    123

--- app/src/main/java/net/diskrom/shiguang/BrowseImageActivity.java
+++ app/src/main/java/net/diskrom/shiguang/BrowseImageActivity.java
@@ -3,6 +3,11 @@ package net.diskrom.shiguang;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -14,9 +19,12 @@ import android.widget.Toast;
 
 import com.apkfuns.logutils.LogUtils;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+
 
 public class BrowseImageActivity extends BaseActivity {
     private Bitmap srcImageBitmap;  //维护一个存储原图Bitmap的变量
@@ -55,17 +63,40 @@ public class BrowseImageActivity extends BaseActivity {
         //loadSrcImage(path);
         Glide.with(BrowseImageActivity.this).load(path).into(new GlideDrawableImageViewTarget(browseImage) {
             @Override
-            public void onResourceReady(GlideDrawable drawable, GlideAnimation anim) {
+            public void onResourceReady(final GlideDrawable drawable, GlideAnimation anim) {
                 super.onResourceReady(drawable, anim);
                 //在这里添加一些图片加载完成的操作
                 quse.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(BrowseImageActivity.this,"hello",Toast.LENGTH_LONG).show();
+                        //1、从drawable 转化为 bitmap
+
+                        //2、直接从ImageView控件中获取bitmap
+                        Bitmap bitmapOrigin = ((GlideBitmapDrawable)browseImage.getDrawable()).getBitmap();
+                        int height = bitmapOrigin.getHeight();
+                        int width = bitmapOrigin.getWidth();
+                        //定义灰度图像
+                        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                        Canvas canvas = new Canvas(bmpGrayscale);
+                        Paint paint = new Paint();
+                        ColorMatrix cm = new ColorMatrix();
+                        cm.setSaturation(0);
+                        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+                        paint.setColorFilter(f);
+                        canvas.drawBitmap(bitmapOrigin, 0, 0, paint);
+                        browseImage.setImageBitmap(bmpGrayscale);
                     }
                 });
             }
         });
+
+        /*Glide.with(BrowseImageActivity.this).load(path).asBitmap().into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap bitmap, GlideAnimation anim) {
+                // setImageBitmap(bitmap) on CircleImageView
+                browseImage.setImageBitmap(bitmap);
+            }
+        });*/
     }
 
     private void init(){
@@ -86,4 +117,6 @@ public class BrowseImageActivity extends BaseActivity {
             }
         }).start();
     }
+
+
 }
