0610b7399fa598e6239f6a984d854aa304bb3d88
unknown <ekuaibao1@163.com>
2017-02-18 14:28:51 +0800

    123

--- matrix.py
+++ matrix.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-class matrix:
-	''''计算乘方  base 底数 exponent 指数'''
+from math import Math
+# 矩阵计算库
+class Matrix:
+	#''''计算乘方  base 底数 exponent 指数'''
 	def mathPower(base,exponent):
         	result = 1;
         	i=1;
@@ -11,7 +13,7 @@ class matrix:
             		i=i+1;
         	return result;
 
-        '''print matrix'''
+        #'''打印矩阵'''
         def matrixPrint(self,matrix):
         	print '['
         	for i in matrix:
@@ -20,7 +22,7 @@ class matrix:
         		print i;
         	print ']'
 
-        '''去除一维列表中的重复值'''
+        #'''去除一维列表中的重复值'''
         def  array_unique(self,array):
         	array_ = [];
         	for i in array:
@@ -28,7 +30,7 @@ class matrix:
         			array_.append(i);
         	return array_;
 
-       	'''去除多维列表中的重复值'''
+       	#'''去除多维列表中的重复值'''
        	def multi_array_unique(self,array):
        		array_ = self.array_unique(array);
        		for i in range(len(array_)):
@@ -37,7 +39,7 @@ class matrix:
        		array__ = self.array_unique(array_);
        		return array__;
 
-       	'''验证输入的python列表是否能表示一个合法的矩阵 验证成功返回一个由行数和列数组成的两个元素的一维python列 表 验证失败返回false'''
+       	#'''验证输入的python列表是否能表示一个合法的矩阵 验证成功返回一个由行数和列数组成的两个元素的一维python列 表 验证失败返回false'''
        	def validMatrix(slef,matrix):
        		if isinstance(matrix,(list)) == False:
        			return False;
@@ -51,10 +53,80 @@ class matrix:
 				if len(matrix[i]) != len(matrix[i-1]):
 					return False;
 		return [length,len(matrix[0])];
-	
-matrix = matrix()
+	#'矩阵的和'
+	def matrixPlus(self,matrix1,matrix2):
+		matrix1_info = self.validMatrix(matrix1) ;
+		matrix2_info = self.validMatrix(matrix2) ;
+		if (matrix1_info  == False) or (matrix1_info == False):
+			print 'paramters is invalid';
+			return False;
+		if(matrix1_info[0] != matrix2_info[0]) or (matrix1_info[1] != matrix2_info[1]):
+			print 'can\'t plus';
+			return False;
+		matrix3 = matrix1;
+		for i in range(matrix1_info[0]):
+			for j in range(matrix1_info[1]):
+				matrix3[i][j] = int(matrix1[i][j]) + int(matrix2[i][j]);
+		return matrix3;
+
+	#'矩阵的差'
+	def matrixMinus(self,matrix1,matrix2):
+		matrix1_info = self.validMatrix(matrix1) ;
+		matrix2_info = self.validMatrix(matrix2) ;
+		if (matrix1_info  == False) or (matrix1_info == False):
+			print 'paramters is invalid';
+			return False;
+		if(matrix1_info[0] != matrix2_info[0]) or (matrix1_info[1] != matrix2_info[1]):
+			print 'can\'t minus';
+			return False;
+		matrix3 = matrix1;
+		for i in range(matrix1_info[0]):
+			for j in range(matrix1_info[1]):
+				matrix3[i][j] = int(matrix1[i][j]) - int(matrix2[i][j]);
+		return matrix3;
+
+	#'矩阵的积'
+	def matrixMulti(self,matrix1,matrix2):
+		matrix1_info = self.validMatrix(matrix1) ;
+		matrix2_info = self.validMatrix(matrix2) ;
+		if (matrix1_info  == False) or (matrix1_info == False):
+			print 'paramters is invalid';
+			return False;
+		if matrix1_info[1] != matrix2_info[0]:
+			print 'can\'t multi';
+			return False;
+		#matrix3 = [[0]*matrix2_info[1]]*matrix1_info[0];
+		matrix3 = [[0 for i in range(matrix2_info[1])] for i in range(matrix1_info[0])];
+		for i in range(matrix1_info[0]):
+			for j in range(matrix2_info[1]):
+				#temp = 0;
+				for k in range(matrix1_info[1]):
+					matrix3[i][j]  += int(matrix1[i][k]) * int(matrix2[k][j]);
+				#matrix3[i][j] = temp;
+				#print temp;
+		return matrix3;
+
+	#'矩阵反转 行和列的元素互换'
+	def matrixReverse(self,matrix):
+		matrix_info = self.validMatrix(matrix);
+		if matrix_info == False:
+			print 'paramters is invalid';
+			return False;
+		matrix_  = [[0 for i in range(matrix_info[0])] for i in range(matrix_info[1])]
+		for i in range(matrix_info[1]):
+			for j in range(matrix_info[0]):
+				matrix_[i][j] = matrix[j][i];
+		return matrix_;
+
+#matrix = Matrix();
+#result = matrix.matrixMulti([['1','2'],['3','4']],[['1','2'],['3','4']]);
 #matrix_.matrixPrint([['a','b','c'],['a','b','c'],['a','b','c']]);
 #print matrix.multi_array_unique([['a','b','b','c'],['a','b','c','c']]);
-print matrix.validMatrix([['a','b'],['b','c']]);
+#print matrix.validMatrix([['a','b'],['b','c']]);
 #list = ['a','b','c'];
 #print ','.join(list);
+#result = matrix.matrixReverse([['1','2'],[3,4],[5,6]]);
+
+#matrix.matrixPrint(result);
+math  = Math();
+print math.listInverseNum([1,4,2,3,5]);
